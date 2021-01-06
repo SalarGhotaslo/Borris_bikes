8c499803af169a2e51d605db43b9953216f6d037
Katrina Harradine <katrinaharradine@gmail.com>
2021-01-06 16:16:59 +0000

    Step 10 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,10 @@
   describe DockingStation do
 #Use expect for Method - might not need this bit yet but have added in
       it { expect(subject).to respond_to(:release_bike) }
+      #gets bike
+      it 'releases working bikes' do
+        bike = subject.release_bike
+        expect(bike).to be_working
+      end
+      #bike working?
   end
