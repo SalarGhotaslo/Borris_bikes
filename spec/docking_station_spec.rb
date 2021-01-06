a7fe5cd38996ceb4c6441054c40d9f1ca500f899
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-06 17:13:29 +0000

    step 11 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,5 +9,15 @@
         bike = subject.release_bike
         expect(bike).to be_working
       end
-      #bike working?
+      it 'docks something' do
+        bike = Bike.new
+        # We want to return the bike we dock
+        expect(subject.dock(bike)).to eq bike
+      end
+      it 'returns docked bikes' do
+        bike = Bike.new
+        subject.dock(bike)
+        # Again, we need to return the bike we just docked
+        expect(subject.bike).to eq bike
+      end
   end
